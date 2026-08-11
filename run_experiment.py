395a1d3dc1876be2690eba9cbf5f5ff137a265dc
hapinza <lee773@uwm.edu>
2026-08-11 18:44:41 -0500

    Create run_experiment.py

---
+++ run_experiment.py
@@ -0,0 +1,10 @@
+from environment import CameraEnvironment
+
+env = CameraEnvironment()
+
+try:
+    for _ in range(30):
+        result = env.step()
+        print(result)
+finally:
+    env.close()
